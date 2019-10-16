f6d9fc020936782546287e0bcd1d568b3d0a3b04
Adam Caron <adamwcaron@gmail.com>
2019-10-16 15:56:24 -0400

    display the current question title

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
-const Sample = () => <div>test</div>
+const questions = [
+  {
+    title: 'What is your fav color?'
+  },
+  {
+    title: 'What is your fav food?'
+  },
+  {
+    title: 'What is your fav car?'
+  }
+]
+
+class Sample extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentQuestion: 0
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>{questions[this.state.currentQuestion].title}</h3>
+      </div>
+    )
+  }
+}
 
 export default Sample
