7e5cabc6f92c86cd17afb0dbbb47136936b7ccde
Adam Caron <adamwcaron@gmail.com>
2019-10-16 18:29:47 -0400

    store question input data

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const questions = [
   {
-    title: 'What is your fav color?'
+    title: 'What is your fav color?',
   },
   {
     title: 'What is your fav food?'
@@ -19,18 +19,22 @@ class Sample extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      currentQuestionIdx: 0
+      currentQuestionIdx: 0,
+      answers: {}
     }
+    this.answerInput = React.createRef()
   }
 
   nextQ = () => {
     if (this.state.currentQuestionIdx < questions.length - 1) {
+      this.answerInput.value = this.state.answers[this.getQuestionId()] || ''
       return this.setState({ currentQuestionIdx: this.state.currentQuestionIdx + 1 })
     }
   }
 
   prevQ = () => {
     if (this.state.currentQuestionIdx > 0) {
+      this.answerInput.value = this.state.answers[this.getQuestionId()] || ''
       return this.setState({ currentQuestionIdx: this.state.currentQuestionIdx - 1 })
     }
   }
@@ -39,12 +43,31 @@ class Sample extends React.Component {
     return this.state.currentQuestionIdx === questions.length - 1
   }
 
+  getQuestionId = () => {
+    return `question-${this.state.currentQuestionIdx + 1}`
+  }
+
+  handleInput = (e) => {
+    const answers = { ...this.state.answers }
+    answers[e.target.name] = e.target.value
+    this.setState({ answers })
+  }
+
   render () {
+    const questionId = this.getQuestionId()
+    console.log('this.state: ', this.state)
     return (
       <div>
         <button onClick={this.prevQ}>Previous Question</button>
         <button onClick={this.nextQ}>{this.isLastQ() ? 'Finish' : 'Next Question' }</button>
         <h3>{questions[this.state.currentQuestionIdx].title}</h3>
+        <input
+          type="text"
+          name={questionId}
+          onChange={this.handleInput}
+          ref={el => this.answerInput = el}
+          value={this.state.answers[questionId] || ''}
+        />
       </div>
     )
   }
