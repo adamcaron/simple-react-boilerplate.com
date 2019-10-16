e4619b02f0911db1bca6b10fa3edbcc7672eac32
Adam Caron <adamwcaron@gmail.com>
2019-10-16 15:37:46 -0400

    up and running

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,5 +1,49 @@
 import React from 'react'
 
-const Sample = () => <div>test</div>
+// const nextQuestion = (currentQuestion) => {
+// }
+
+const questions = [
+  {
+    title: 'What is your fav color?'
+  },
+  {
+    title: 'What is your fav food?'
+  },
+  {
+    title: 'What is your fav car?'
+  }
+]
+
+class Sample extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentQuestion: 0
+    }
+  }
+
+  // function nextQuestion = (questions) => {
+  //   // if (currentQuestion !== questions.length) {
+  //     // getQuestionData(currentQuestion + 1)
+  //   // }
+  //   // submit
+  //   this.setState(() => { currentQuestion: currentQuestion + 1 })
+  // }
+
+  render () {
+    return (
+      <div>
+        {/* subheader */}
+        {/*   back/forward */}
+        {/*   numbers */}
+        <h3>{questions[this.state.currentQuestion].title}</h3>
+        {/* input */}
+        {/* next-question(currentQuestion) */}
+        {/* <button onClick={this.nextQuestion(questions)}>Next Question</button> */}
+      </div>
+    )
+  }
+}
 
 export default Sample
